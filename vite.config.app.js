bbf19ffb7c34bf5d7a708f5ee490afba35d1ed0b
VeronicaDiazTaylor <151398472+VeronicaDiazTaylor@users.noreply.github.com>
2025-12-29 14:21:46 +0900

    Keystone 3.0.0とconfig

--- vite.config.app.js
+++ vite.config.app.js
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import keystonePlugin from 'keystonemc/vite-plugin';
 
 export default defineConfig({
-  plugins: [keystonePlugin({ uuid: "82b9174d-ea3d-42ae-986c-b42ce3644760" })],
+  plugins: [keystonePlugin({ 
+    name: "WorldEdit",
+    description: "Tool of management your world.",
+    authors: [ "VeronicaDiazTaylor" ],
+    version: [ 1, 0, 0 ],
+    uuid: "82b9174d-ea3d-42ae-986c-b42ce3644760"
+  })],
 });
